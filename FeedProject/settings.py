f5021aea454198dac7d84cade7dacb66dda964dd
juliettesav <juliettesav@outlook.com>
2022-12-13 23:09:25 -0600

    setting up Heroku

--- FeedProject/settings.py
+++ FeedProject/settings.py
@@ -132,3 +132,8 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = 'users:login'
+
+
+# Heroku settings.
+import django_heroku 
+django_heroku.settings(locals())
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn FeedProject.wsgi --log-file -
